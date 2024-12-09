32aba4a22c7b93afc277d85edfc2839e0790be8b
felipe-salcedo <felipesalcedo,ramalho@gmail.com>
2024-12-09 09:07:54 -0300

    projeto zipado

--- CadCliente/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CadCliente.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
